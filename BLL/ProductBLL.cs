2735d98204eb32cf6552a04587bb622d83cf0109
malka-feldman <121830106+malka-feldman@users.noreply.github.com>
2023-01-02 14:28:55 -0800

    Add files via upload

---
+++ BLL/ProductBLL.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using Entities;
+using OOPPROJECT1;
+
+namespace BLL
+{
+    public class ProductBLL
+    {
+        public ProductDAL prod;
+
+        public ProductBLL(ProductDAL prod)
+        {
+            this.prod = prod;
+        }
+
+        public ProductBLL()
+        {
+            prod = new ProductDAL();
+        }
+        public string printBLLList()
+        {
+            string str = prod.printList();
+            return str;
+        }
+
+        public Product Create(Product item)
+        {
+            try
+            {
+                return prod.Create(item);
+            }
+            catch(ProdAlreadyExistsException pe)
+            {
+                throw pe;
+            }
+        }
+
+        public Product Retrieve(int productNum)
+        {
+            return prod.Retrieve(productNum);
+        }
+
+        public bool Update(Product item)
+        {
+            return prod.Update(item);
+        }
+        public void Delete(int productNum)
+        {
+            prod.Delete(productNum);
+        }
+
+    }
+}
